2153e7816b4fdce5e5f25a1b6a7d051fc8414fc0
koala <rproba@list.ru>
2020-11-04 20:44:24 +0300

    some work with js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function (){
     let password = document.getElementById("inputPasswordSignUp"),
         confirmPassword = document.getElementById("inputPasswordRepeatSignUp");
     function checkPassword(){
-        let check = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/;
+        let check = /^(?=.*[A-Z].*[A-Z])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{6,}$/;
         if(!password.value.match(check)){
-            password.setCustomValidity("Wrong password format" + "Your password must contain from 6 to 20 characters, have Latin characters in lower and upper case and at least 1 number");
+            password.setCustomValidity("Wrong password format. " + "Your password must contain from 6 to 20 characters, have Latin characters in lower and upper case and at least 1 number");
         } else{
             password.setCustomValidity('');
         }
